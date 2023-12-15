c4351bd2822857b654edc6ea07279a73aeb4826b
jean keller <jeankellerpessoal@gmail.com>
2023-12-15 11:03:11 -0300

    Exercicio Jquery

---
+++ 02-Modulo-20-JQuery/js/custom.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
+    let titulos = $('h4') // tag
+   
+    let itens = $('.featured-item') // class
+    
+    let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+    // Configuração de produtos
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+     
+     $('.featured-item h4').dblclick( function(){
+
+        $(this).css({
+            'color': '#f00',
+            'background': '#ff0',
+            'font-weight': '100',
+        });
+
+     });
+
+     $('.featured-item a').on('blur', function(event){
+
+        event.preventDefault();
+
+        alert('Produto esgotado');
+
+     })
+
+})
+
+$(document).ready(function() {
+   $(".animacao-imagem").click(function() {
+       $(this).animate({ opacity: 0.5 }, 1000, function() {
+           $(this).animate({ opacity: 1 }, 1000);
+       });
+   });
+});
+
